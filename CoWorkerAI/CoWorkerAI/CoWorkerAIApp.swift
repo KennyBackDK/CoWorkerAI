d01965e082a15e7b952fbbd4e4d708dba1546e80
Kenny Back <foto@kennyback.com>
2025-09-24 07:06:51 +0200

    feat(macOS): add app + set bundle id com.kennyback.coworkerai (shared scheme)

---
+++ CoWorkerAI/CoWorkerAI/CoWorkerAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoWorkerAIApp.swift
+//  CoWorkerAI
+//
+//  Created by Kenny Back on 24/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoWorkerAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
